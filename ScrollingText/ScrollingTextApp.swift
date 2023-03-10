4ed62715f8ec67dbaef20ec60b7625f7e3f1b568
adrianorezena <adrianocr@gmail.com>
2023-03-10 18:31:06 -0300

    Initial Commit

---
+++ ScrollingText/ScrollingTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingTextApp.swift
+//  ScrollingText
+//
+//  Created by Adriano Rezena on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollingTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
